d7cb6452b4dfa9d76a85ac7fc0accdf0bd4a9269
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2022-07-04 02:05:53 +0300

    Fix int and float hook defaults

--- msgpack/msgint.py
+++ msgpack/msgint.py
@@ -14,3 +14,27 @@ class msgByte(msgInt8): pass
 
 class msgFloat(float): pass
 class msgDouble(float): pass
+
+
+MSG_INT_TYPES = [
+    msgUInt8,
+    msgInt8,
+    msgUInt16,
+    msgInt16,
+    msgUInt32,
+    msgInt32,
+    msgUInt64,
+    msgInt64,
+    msgUByte,
+    msgByte,
+    msgFloat,
+    msgDouble,
+]
+
+
+def msg_int_hook(obj, val):
+    return MSG_INT_TYPES[val](obj)
+
+
+def msg_float_hook(obj, val):
+    return MSG_INT_TYPES[val](obj)
